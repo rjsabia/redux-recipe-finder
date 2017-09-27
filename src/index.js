83556fa25ab4d1dd08b5dea714cd0deba349ec7e
Russell Sabia <rjsabia@gmail.com>
2017-09-27 01:05:15 -0500

    initial commit, form built, store, action, reducer setup,API tested AOK

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import App from './components/App';
+
+const store = createStore(rootReducer);
+store.subscribe(() => console.log('store', store.getState()));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root'))
